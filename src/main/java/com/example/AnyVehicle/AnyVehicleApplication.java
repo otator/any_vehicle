fd40a2407827acaa636b0560e21cdf1640192fca
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-09-09 17:38:02 +0300

    add spring files

---
+++ src/main/java/com/example/AnyVehicle/AnyVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnyVehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnyVehicle/AnyVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnyVehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
